8404775f918d9b58366caea76281886f60be5ae3
Alex Woersching <ajw006@gmail.com>
2020-11-09 04:33:16 -0800

    Adds more queries to q.sql

--- q.sql
+++ q.sql
@@ -37,3 +37,38 @@ FROM
 WHERE (d_address = h_address) AND 
     (d_devtype = 'laptop') AND 
     (d_devname LIKE 'C%')
+
+-- #6
+SELECT DISTINCT h_address, cpl_conname, co_ispname, cpl_locname, s_speed
+FROM
+    contractsoff,
+    contractsperloc,
+    speed,
+    house
+WHERE (co_conname = cpl_conname) AND 
+    (cpl_locname = s_locname) AND 
+    (h_locname = cpl_locname) AND 
+    (h_address = (SELECT h_address 
+                FROM house LIMIT 1))
+ORDER BY h_address;
+
+-- #7
+select co_ispname, avg(n_speed), avg(n_price) 
+from network, contractsoff
+where co_conname = n_conname
+group by co_ispname
+
+-- #8
+select h_locname, max(speed) 
+from 
+    (select h_locname, avg(n_speed) as speed, avg(n_price) 
+     from network, house 
+     where (h_address = n_address)
+     group by h_locname
+    )SQ
+
+-- #9
+select h_address , count(cpl_conname) 
+from contractsperloc, house
+where cpl_locname = h_locname
+group by h_address
