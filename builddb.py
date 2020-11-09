e9e07d24aff2ee5cb785f54f970bb0495022acdf
Alex Woersching <ajw006@gmail.com>
2020-11-08 19:28:32 -0800

    Populates house, location, and makes dropTable()

--- builddb.py
+++ builddb.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random as r
 from sqlite3 import Error
 import pandas as pd
 
@@ -14,9 +15,9 @@ def createTables(_conn):
         c.execute("""CREATE TABLE IF NOT EXISTS house(
             h_address varchar(255) not null,
             devicecount int not null,
-            h_conname varchar(255) not null,
+            h_conname int not null, 
             h_locname varchar(255) not null
-            )""")
+            )""") # Changed h_conname to be an int to represent the contract index, change back if necessary
         print('tmade')
         c.execute("""CREATE TABLE IF NOT EXISTS contractsoff(
             co_ispname varchar(255) not null,
@@ -55,14 +56,71 @@ def createTables(_conn):
     except Error as e:
         print(e)
 
+def dropTables(_conn):
+    try:
+        sql = "DROP TABLE location"
+        _conn.execute(sql)
+        sql = "DROP TABLE house"
+        _conn.execute(sql)
+        sql = "DROP TABLE contractsoff"
+        _conn.execute(sql)
+        sql = "DROP TABLE ISP"
+        _conn.execute(sql)
+        sql = "DROP TABLE contractsperloc"
+        _conn.execute(sql)
+        sql = "DROP TABLE devices"
+        _conn.execute(sql)
+        sql = "DROP TABLE network"
+        _conn.execute(sql)
+        sql = "DROP TABLE speed"
+        _conn.execute(sql)
+
+        _conn.commit()
+    except Error as e:
+        print(e)
+
+def populateTables(_conn):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Populate table Warehouse")
+    try:
+        c = _conn.cursor()
+        isps = ['Banana Republic Wireless', 'Bojangles Express', 'Pineapple Inc', 'ATAT Wireless', 'Abar Internet']
+        cities = ['Merced', 'Los Angeles', 'San Diego', 'Tokyo', 'Moscow']
+        devices = ['laptop', 'pc', 'tablet', 'phone', 'console']
+        speeds = [10, 50, 100, 250, 1000]
+
+        sql = "INSERT INTO location VALUES (?, ?)"
+        for i in cities:
+            num = r.randint(0,2)
+            for j in range(3): # 3 ISP's per city
+                isp = isps[num + j]
+                c.execute(sql, (i, isp))
+                print("Inserting: (" + i + ", " + isp + ") into: location")
+
+        sql = "INSERT INTO house VALUES (?, ?, ?, ?)"
+        for i in cities:
+            address = "Address___#" + str(r.randint(0,99999))
+            count = r.randint(1,20) # 1-20 devices in a house
+            contract = r.randint(1,10) # Refers to the contract number/index
+            for j in range(3): # 3 houses per city
+                c.execute(sql, (str(address), count, contract, i))
+                print("Inserting: (" + address + ", " + str(count) + ", " + str(contract) + ", " + i + ") into : house")
+        _conn.commit()
+                    
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
 def main():
     database = r"proj.sqlite"
 
     # create a database connection
     conn = sqlite3.connect(database)
     with conn:
+        dropTables(conn)
         createTables(conn)
-        #populateTables(conn)
+        populateTables(conn)
 
     conn.close()
 
