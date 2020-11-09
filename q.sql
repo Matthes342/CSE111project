417f92415c1cb34ba2910dee41cb075781c850e0
Alex Woersching <ajw006@gmail.com>
2020-11-09 05:08:46 -0800

    Further adds more queries

--- q.sql
+++ q.sql
@@ -36,7 +36,7 @@ FROM
     house
 WHERE (d_address = h_address) AND 
     (d_devtype = 'laptop') AND 
-    (d_devname LIKE 'C%')
+    (d_devname LIKE 'C%');
 
 -- #6
 SELECT DISTINCT h_address, cpl_conname, co_ispname, cpl_locname, s_speed
@@ -56,7 +56,7 @@ ORDER BY h_address;
 select co_ispname, avg(n_speed), avg(n_price) 
 from network, contractsoff
 where co_conname = n_conname
-group by co_ispname
+group by co_ispname;
 
 -- #8
 select h_locname, max(speed) 
@@ -65,10 +65,55 @@ from
      from network, house 
      where (h_address = n_address)
      group by h_locname
-    )SQ
+    )SQ;
 
 -- #9
 select h_address , count(cpl_conname) 
 from contractsperloc, house
 where cpl_locname = h_locname
-group by h_address
+group by h_address;
+
+-- #10
+SELECT n_conname
+FROM(
+SELECT n_conname, MAX(n_price)
+FROM network
+)SQ;
+
+-- #11
+SELECT n_conname
+FROM(
+SELECT n_conname, MIN(n_price)
+FROM network
+)SQ;
+
+-- #12
+SELECT n_conname
+FROM(
+    SELECT n_conname, MAX(num)
+    FROM(
+        SELECT n_conname, COUNT(*) AS num
+        FROM network
+        GROUP BY n_conname
+    )SQ1
+)SQ2;
+
+-- #13
+SELECT s_locname, AVG(s_speed)
+FROM speed
+GROUP BY s_locname;
+
+-- #14
+SELECT h_locname
+FROM(
+    SELECT h_locname, MAX(c)
+    FROM(
+        SELECT h_locname, IFNULL(COUNT(d_devtype), 0) AS c
+        FROM 
+            devices,
+            house
+        WHERE (d_address = h_address) AND 
+            (d_devtype = 'phone')
+        GROUP BY h_locname
+    )SQ
+)SQ1;
